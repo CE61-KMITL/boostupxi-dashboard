912461d7f20c185a2327b291ab05562b8dfe3214
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2023-02-01 13:16:05 +0700

    style(all-task): format all-task page

--- src/pages/all-tasks.tsx
+++ src/pages/all-tasks.tsx
@@ -12,32 +12,32 @@ const TasksPage: NextPage = () => {
   return (
     <Fragment>
       <Navbar />
-      <div className="container mx-auto flex justify-center items-baseline mt-8 gradient-radial-bg grow min-h-screen">
+      <div className="gradient-radial-bg container mx-auto mt-8 flex min-h-screen grow items-baseline justify-center">
         <div className="stars"></div>
         <div className="stars2"></div>
         <div className="stars3"></div>
-        <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400 z-20 rounded-lg overflow-hidden">
+        <table className="z-20 w-full overflow-hidden rounded-lg text-left text-sm text-gray-500 dark:text-gray-400">
           <thead className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
             <tr className="text-center">
-              <th scope="col" className='py-3 px-2'>
+              <th scope="col" className="py-3 px-2">
                 Task ID
               </th>
-              <th scope="col" className='py-3 px-2'>
+              <th scope="col" className="py-3 px-2">
                 Task Name
               </th>
-              <th scope="col" className='py-3 px-2'>
+              <th scope="col" className="py-3 px-2">
                 Author
               </th>
-              <th scope="col" className='py-3 px-2'>
+              <th scope="col" className="py-3 px-2">
                 Levels
               </th>
-              <th scope="col" className='py-3 px-2'>
+              <th scope="col" className="py-3 px-2">
                 Tags
               </th>
-              <th scope="col" className='py-3 px-2'>
+              <th scope="col" className="py-3 px-2">
                 Status
               </th>
-              <th scope="col" className='py-3 px-2'>
+              <th scope="col" className="py-3 px-2">
                 Action
               </th>
             </tr>
