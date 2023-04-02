507b3e2557cd47cfbf4b1c8dd4d91e19201d1377
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2023-04-02 17:52:52 +0700

    feat: simple search

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -4,18 +4,41 @@ import { TaskTable } from '@/components';
 import Layouts from '@/layouts/Layouts';
 import { useState, useEffect } from 'react';
 import { getTasksData } from '@/services/task.services';
+import Fuse from 'fuse.js';
 
 const TasksPage: NextPage = () => {
-  const [taskData, setTaskData] = useState([]);
+  const [taskData, setTaskData] = useState<ITask[]>([]);
   const [currentPage, setCurrentPage] = useState<number>(1);
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [filteredTasks, setFilteredTasks] = useState<ITask[]>([]);
+  const [fuse, setFuse] = useState<Fuse<ITask>>();
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await getTasksData(currentPage);
       setTaskData(response);
+      setFilteredTasks(response); // set the initial value of filteredTasks to the fetched data
+      setFuse(
+        new Fuse(response, {
+          keys: ['title', 'description', 'author', 'level', 'tags'],
+        }),
+      );
     };
     fetchData();
   }, [currentPage]);
+
+  useEffect(() => {
+    if (fuse && searchTerm) {
+      const results = fuse.search(searchTerm);
+      setFilteredTasks(results.map((result) => result.item));
+    } else {
+      setFilteredTasks(taskData);
+    }
+  }, [searchTerm, taskData, fuse]);
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
 
   const nextPage = () => {
     setCurrentPage(currentPage + 1);
@@ -33,6 +56,15 @@ const TasksPage: NextPage = () => {
       <div className="stars2"></div>
       <div className="stars3"></div>
       <div className="container mx-auto mt-20 overflow-auto rounded-lg py-12 px-6">
+        <div className="mb-4 flex flex-row justify-center">
+          <input
+            type="text"
+            placeholder="Search task by name"
+            value={searchTerm}
+            onChange={handleSearch}
+            className="w-full rounded-md border px-4 py-2 text-sm shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
         <table className="mx-auto my-auto w-full text-sm text-gray-500 shadow-md dark:text-gray-400">
           <thead className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
             <tr>
@@ -60,26 +92,25 @@ const TasksPage: NextPage = () => {
             </tr>
           </thead>
           <tbody>
-            {taskData &&
-              taskData.map((val: ITask) => (
-                <TaskTable
-                  key={val._id}
-                  _id={val._id}
-                  title={val.title}
-                  description={val.description}
-                  author={val.author}
-                  level={val.level}
-                  status={val.status}
-                  tags={val.tags}
-                  hint={val.hint}
-                  files={val.files}
-                  testcases={val.testcases}
-                  solution_code={val.solution_code}
-                  created_at={val.created_at}
-                  updated_at={val.updated_at}
-                  __v={val.__v}
-                />
-              ))}
+            {filteredTasks.map((val: ITask) => (
+              <TaskTable
+                key={val._id}
+                _id={val._id}
+                title={val.title}
+                description={val.description}
+                author={val.author}
+                level={val.level}
+                status={val.status}
+                tags={val.tags}
+                hint={val.hint}
+                files={val.files}
+                testcases={val.testcases}
+                solution_code={val.solution_code}
+                created_at={val.created_at}
+                updated_at={val.updated_at}
+                __v={val.__v}
+              />
+            ))}
           </tbody>
         </table>
         <div className="mx-auto mt-3 ml-3 flex flex-row justify-end">
