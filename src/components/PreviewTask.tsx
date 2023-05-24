3777bb4959470b861028ef41adcff031eda0da06
1tpp <panapat.dev@gmail.com>
2023-05-24 20:43:24 +0700

    style(PreviewTask.tsx): change background color of code blocks to bg-neutral-800 and remove unnecessary classes from comment button and comment container.

--- src/components/PreviewTask.tsx
+++ src/components/PreviewTask.tsx
@@ -350,7 +350,7 @@ const PreviewTask = ({ id, isOpen, onClose }: Props) => {
                             Task Input {index + 1}
                           </p>
                           <code
-                            className={`${kanit.className} mt-2 h-20 overflow-auto whitespace-pre-wrap rounded-md bg-slate-500 px-2 py-2 text-base text-gray-100`}
+                            className={`${kanit.className} mt-2 h-20 overflow-auto whitespace-pre-wrap rounded-md bg-neutral-800 px-2 py-2 text-base text-gray-100`}
                           >
                             {val.input ? val.input : 'No Input'}
                           </code>
@@ -360,7 +360,7 @@ const PreviewTask = ({ id, isOpen, onClose }: Props) => {
                             Task Output {index + 1}
                           </p>
                           <code
-                            className={`${kanit.className} mt-2 h-20 overflow-y-scroll whitespace-pre-wrap rounded-md bg-slate-500 px-2 py-2 text-base text-gray-100`}
+                            className={`${kanit.className} mt-2 h-20 overflow-y-scroll whitespace-pre-wrap rounded-md bg-neutral-800 px-2 py-2 text-base text-gray-100`}
                           >
                             {val.output ? val.output : 'No Output'}
                           </code>
@@ -368,7 +368,7 @@ const PreviewTask = ({ id, isOpen, onClose }: Props) => {
                         <div className="shadow-3xl shadow-shadow-500 !bg-navy-700 flex flex-col justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 shadow-none">
                           <p className="text-sm text-gray-600">Published</p>
                           <p
-                            className={`${kanit.className} mt-2 h-20 overflow-auto whitespace-pre-wrap px-2 text-base text-gray-600`}
+                            className={`${kanit.className} mt-2 h-20 overflow-auto whitespace-pre-wrap  text-base text-gray-600`}
                           >
                             {val.published ? 'Published' : 'Not Published'}
                           </p>
@@ -448,42 +448,44 @@ const PreviewTask = ({ id, isOpen, onClose }: Props) => {
                         }}
                       ></textarea>
                     </div>
-                    <button
-                      type="submit"
-                      className=" focus:ring-primary-200  hover:bg-primary-800 inline-flex items-center rounded-lg bg-blue-700 py-2.5 px-4 text-center text-base font-medium text-white focus:ring-4"
-                      onClick={handleSubmitComment}
-                      disabled={!commentMessage}
-                    >
-                      {loading ? (
-                        <div className="text-center">
-                          <div role="status">
-                            <svg
-                              aria-hidden="true"
-                              className="mr-2 inline h-8 w-8 animate-spin fill-blue-600 text-gray-200"
-                              viewBox="0 0 100 101"
-                              fill="none"
-                              xmlns="http://www.w3.org/2000/svg"
-                            >
-                              <path
-                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                                fill="currentColor"
-                              />
-                              <path
-                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                                fill="currentFill"
-                              />
-                            </svg>
+                    <div className="flex justify-end">
+                      <button
+                        type="submit"
+                        className=" focus:ring-primary-200  hover:bg-primary-800 inline-flex items-center rounded-lg bg-blue-700 py-2.5 px-4 text-center text-base font-medium text-white focus:ring-4"
+                        onClick={handleSubmitComment}
+                        disabled={!commentMessage}
+                      >
+                        {loading ? (
+                          <div className="text-center">
+                            <div role="status">
+                              <svg
+                                aria-hidden="true"
+                                className="mr-2 inline h-8 w-8 animate-spin fill-blue-600 text-gray-200"
+                                viewBox="0 0 100 101"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path
+                                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                  fill="currentColor"
+                                />
+                                <path
+                                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                  fill="currentFill"
+                                />
+                              </svg>
+                            </div>
                           </div>
-                        </div>
-                      ) : (
-                        'Post Comment'
-                      )}
-                    </button>
+                        ) : (
+                          'Post Comment'
+                        )}
+                      </button>
+                    </div>
                   </form>
                   {comments &&
                     comments.map((val: IComment) => (
                       <div key={val.id}>
-                        <article className="mb-6 rounded-lg bg-white p-6 text-base ">
+                        <article className="mb-6 rounded-lg bg-white text-base ">
                           <footer className="mb-2 flex items-center justify-between">
                             <div className="flex items-center">
                               <p className="mr-3 items-center text-base text-gray-900 sm:inline-flex sm:text-xl ">
