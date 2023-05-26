f04017b8ba3c99cf1cc9fd95fcd108cb93935db0
khris-xp <khrisbharmmano@gmail.com>
2023-05-26 16:00:20 +0700

    fix: preview modal

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -113,7 +113,7 @@ const TasksPage: NextPage = () => {
         <LoadingFile />
       ) : (
         <div className="container mx-auto mt-14 min-h-screen overflow-auto rounded-lg py-12 px-6">
-          <table className="mx-auto my-auto w-full overflow-hidden rounded-lg border-none bg-white bg-opacity-5 text-sm text-gray-400 shadow-md backdrop-blur-sm">
+          <table className="mx-auto my-auto w-full overflow-hidden rounded-lg border-none bg-white bg-opacity-5 text-sm text-gray-400 shadow-md backdrop-filter">
             <thead className="bg-gray-700 text-xs uppercase text-gray-100 backdrop-blur-sm">
               <tr>
                 <th scope="col" className="px-6 py-1">
